2b8b1a80b936ed3326f0b88ed2bea7c7b279959c
thomas15v <tvanmellaerts@gmail.com>
2017-03-27 11:18:38 +0200

    Exit redirect server when we need to refresh certificates

--- run.sh
+++ run.sh
@@ -1,5 +1,11 @@
 #!/bin/sh
 
+if [ -f /app/pid ]; then
+    echo "Killing redirect server"
+    kill -9 `cat /app/pid`
+    rm /app/pid
+fi
+
 echo "Refreshing certificates..."
 [ -z "$EMAIL" ] && echo "ERROR: Need to set EMAIL" && exit 1;
 [ -z "$DOMAINS" ] && echo "ERROR: Need to set DOMAINS. Example: 'www.example.com,example.com'" && exit 1;
@@ -10,6 +16,9 @@ do
   domains="$domains -d ${i//[[:blank:]]/}"
 done
 
-echo $domains
-
 certbot certonly --agree-tos --email $EMAIL --noninteractive --standalone --preferred-challenges http-01 $domains
+
+echo "starting redirect server"
+
+nohup python /app/app.py &
+echo $! > /app/pid
--- start.sh
+++ start.sh
@@ -1,5 +1,4 @@
 #!/bin/sh
 sleep 1
 sh /app/run.sh
-crond
-python /app/app.py
+crond -f
